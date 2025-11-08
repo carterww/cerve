60258065aaced2f99fdecf62a3f1c86f89c64222
Carter Williams <carterww@hotmail.com>
2025-11-07 21:18:14 -0600

    http: Add some basic functions for managing requests & parsing them

--- src/cerve_http.c
+++ src/cerve_http.c
@@ -17,11 +17,18 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <cerve_http.h>
 
+#include "cerve_cc.h"
 #include "cerve_debug.h"
+#include "cerve_http_headers.h"
+#include "cerve_http_parse.h"
 #include "cerve_tcp.h"
+
+#define DEFAULT_HEADERS_LEN (24)
 
 struct cerve_http_server_cfg {
 	uint64_t req_read_timeout_ms;
@@ -46,3 +53,155 @@ static const struct cerve_http_server_cfg http_server_cfg_default = {
 	.connections_max = 1024,
 	.socket_backlog = 4096,
 };
+
+static const struct cerve_http_request_internal http_request_internal_default = {
+	.buf = NULL,
+	.parse_pos = NULL,
+	.content_length = 0,
+	.state = CERVE_HTTP_REQUEST_PARSE_STATUS_LINE,
+	.req = (struct cerve_http_request) {
+		.method = CERVE_HTTP_METHOD_INVALID,
+		.version = CERVE_HTTP_VERSION_AUTO,
+		.headers = NULL,
+		.path = NULL,
+		.path_len = 0,
+		.query = NULL,
+		.query_len = 0,
+		.body = NULL,
+		.body_len = 0,
+		.raw = NULL,
+		.raw_len = 0,
+	},
+	.headers_map = {
+		.type = CERVE_HTTP_HEADERS_MAP,
+		.map = { {0} },
+	},
+	.err_ctx = (void *)0,
+};
+
+struct cerve_http_request_internal *
+cerve_http_request_internal_create(const struct cerve_http_server_cfg *cfg)
+{
+	int err;
+
+	cassert(cfg != NULL);
+
+	size_t buf_max_size =
+		cfg->req_headers_size_max + cfg->req_body_size_max;
+	// TODO: Swap this for my virtual arean
+	void *area = malloc(sizeof(struct cerve_http_request_internal) +
+			    buf_max_size);
+	if (area == NULL) {
+		return NULL;
+	}
+
+	struct cerve_http_request_internal *req = area;
+	char *buf = (char *)area + sizeof(*req);
+	memcpy(req, &http_request_internal_default, sizeof(*req));
+	req->buf = buf;
+	req->parse_pos = buf;
+	req->req.headers = &req->headers_map;
+	req->req.raw = buf;
+	req->req.raw_len = buf_max_size;
+	err = cerve_http_headers_map_init(&req->headers_map.map,
+					  DEFAULT_HEADERS_LEN);
+	if (err == 0) {
+		return req;
+	}
+	free(area);
+	return NULL;
+}
+
+int cerve_http_request_internal_destroy(struct cerve_http_request_internal *req)
+{
+	int err;
+
+	cassert(req != NULL);
+	cassert(req->buf != NULL);
+	cassert(req->headers_map.type == CERVE_HTTP_HEADERS_MAP);
+
+	err = cerve_http_headers_map_deinit(&req->headers_map.map);
+	if (err) {
+		return err;
+	}
+	// This is pretty cheap and ensures we don't accidentally use old data
+	memcpy(req, &http_request_internal_default, sizeof(*req));
+	// This also frees the buf since they were the same allocation
+	free(req);
+	return 0;
+}
+
+int cerve_http_request_handle_new_data(struct cerve_http_request_internal *req)
+{
+	enum cerve_http_parse_status_line_err sl_err;
+	enum cerve_http_parse_headers_err hdr_err;
+
+	// This is all the way up here so we can check the assertions again
+switch_parse_state:
+	cassert(req != NULL);
+	cassert(req->buf != NULL);
+	cassert(req->parse_pos != NULL);
+	cassert(req->state != CERVE_HTTP_REQUEST_PARSE_DONE);
+	cassert(req->req.headers != NULL);
+	cassert(req->req.raw != NULL);
+	cassert(req->req.raw_len > 0);
+	cassert(req->headers_map.type == CERVE_HTTP_HEADERS_MAP);
+
+	switch (req->state) {
+	case CERVE_HTTP_REQUEST_PARSE_STATUS_LINE:
+		cassert(req->buf == req->parse_pos);
+		cassert(req->content_length == 0);
+		sl_err = cerve_http_parse_status_line(req);
+		switch (sl_err) {
+		case CERVE_HTTP_SL_ERR_OK:
+			req->state = CERVE_HTTP_REQUEST_PARSE_HEADERS;
+			goto switch_parse_state;
+		case CERVE_HTTP_SL_ERR_INCOMPLETE:
+			return 0;
+		case CERVE_HTTP_SL_ERR_BAD_METHOD:
+		case CERVE_HTTP_SL_ERR_BAD_PATH:
+		case CERVE_HTTP_SL_ERR_BAD_VERSION:
+		case CERVE_HTTP_SL_ERR_BAD_LINE:
+			return 400;
+		case CERVE_HTTP_SL_ERR_HTTP2:
+			return 505;
+		default:
+			cpanic();
+			return -1;
+		}
+	case CERVE_HTTP_REQUEST_PARSE_HEADERS:
+		cassert(req->buf < req->parse_pos);
+		cassert(*req->parse_pos != '\r');
+		cassert(*req->parse_pos != '\n');
+		cassert(req->content_length == 0);
+		cassert(req->req.method != CERVE_HTTP_METHOD_INVALID);
+		cassert(req->req.version == CERVE_HTTP_VERSION_10 ||
+			req->req.version == CERVE_HTTP_VERSION_11);
+		cassert(req->req.path != NULL);
+		cassert(req->req.path_len > 0);
+		hdr_err = cerve_http_parse_headers(req);
+		switch (hdr_err) {
+		case CERVE_HTTP_HDR_ERR_OK:
+			req->state = CERVE_HTTP_REQUEST_PARSE_BODY;
+			goto switch_parse_state;
+		case CERVE_HTTP_HDR_ERR_INCOMPLETE:
+			return 0;
+		case CERVE_HTTP_HDR_ERR_MISSING_COLON:
+			CC_ATTR_FALLTHROUGH;
+		case CERVE_HTTP_HDR_ERR_NAME_NOT_TOKEN:
+			return 400;
+		case CERVE_HTTP_HDR_ERR_MAP_ADD_FAIL:
+			return 500;
+		default:
+			cpanic();
+			return -1;
+		}
+	case CERVE_HTTP_REQUEST_PARSE_BODY:
+		return 200;
+	case CERVE_HTTP_REQUEST_PARSE_DONE:
+		CC_ATTR_FALLTHROUGH;
+	default:
+		cpanic();
+		return -1;
+	}
+}
